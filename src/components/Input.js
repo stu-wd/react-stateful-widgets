f5cc0b17f5d97d2ae5dc2133decbd642d5b42b48
Stuart Darsey <stuart.darsey@gmail.com>
2021-03-25 17:06:20 -0400

    programmers.js done, MVP done

--- src/components/Input.js
+++ src/components/Input.js
@@ -54,16 +54,16 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: (inputValue.length > 10) ? "crimson" : "royalblue"
+    color: ( inputValue.length > 10 ) ? "crimson" : "royalblue"
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}> { inputValue.toUpperCase() } </div>
+      <div id='output' style={ style }> { inputValue.toUpperCase() } </div>
       <div>
-        <input id='input' type='text' onChange={changeInput} value={inputValue}/> {/* STEP 6 */}
-        <button id='resetInput' onClick={reset}>Reset</button>
+        <input id='input' type='text' onChange={ changeInput } value={ inputValue }/> {/* STEP 6 */}
+        <button id='resetInput' onClick={ reset }>Reset</button>
       </div>
     </div>
   );
